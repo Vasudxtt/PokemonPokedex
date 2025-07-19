faa3e348219c64acfe6af6f2f8cd5576bb4015e7
Vasudxtt <141990594+Vasudxtt@users.noreply.github.com>
2025-07-20 01:51:41 +0530

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
+import { useState } from "react"
 import { Header } from "./components/Header"
 import { PokeCard } from "./components/PokeCard"
 import { SideNav } from "./components/SideNav"
 
 function App() {
-
+const [selectedPokemon, setSelectedPokemon] = useState
   return (
     <>
     <Header/>
