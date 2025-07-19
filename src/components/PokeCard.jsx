af318237b93b05fa8d1f4dbe01db2020faf9f711
Vasudxtt <141990594+Vasudxtt@users.noreply.github.com>
2025-07-20 02:05:57 +0530

    update

--- src/components/PokeCard.jsx
+++ src/components/PokeCard.jsx
@@ -13,7 +13,7 @@ export function PokeCard(props){
     //check if the selected pokemon information is available in the cache
 
     // 1.define the cache
-
+    let cache = {}
     // 2 check if the slected pokemon is in the cache ,otherwise fetch the pokemon from the api
 
     // 3 if we fetch the api , make sure to save the information to the cache for next time
