c6557b37bef9c33eb3965c8a9673eecc764bf63b
Vasudxtt <141990594+Vasudxtt@users.noreply.github.com>
2025-07-20 01:46:40 +0530

    update

--- src/components/SideNav.jsx
+++ src/components/SideNav.jsx
@@ -6,9 +6,12 @@ import {first151Pokemon, getFullPokedexNumber} from"../utils/index"
 export function SideNav(){
   return(
     <nav>
+      <div className={"header"}>
+        <h1>Pokédex</h1>
+      </div>
       {first151Pokemon.map((pokemon,pokemonIndex)=>{
         return(
-          <button>
+          <button className={'nav-card '}>
             <p>{getFullPokedexNumber(pokemonIndex)}</p>
             <p>{pokemon}</p>
           </button>
