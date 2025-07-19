d197b5a6b5cf74bf1973120bc47a5a18225dd8a2
Vasudxtt <141990594+Vasudxtt@users.noreply.github.com>
2025-07-20 02:01:05 +0530

    update

--- src/components/PokeCard.jsx
+++ src/components/PokeCard.jsx
@@ -1,9 +1,16 @@
-import { useEffect } from "react"
+import { useEffect, useState } from "react"
 
 export function PokeCard(props){
   const {selectedPokemon} = props
+  const[data,setData] = useState(null)
 
-  useEffect(()=>{},[selectedPokemon])
+  useEffect(()=>{
+    // if loading,exit logic
+    //check if the selected pokemon information is available in the cache
+    // 1.define the cache
+    // 2 check if the slected pokemon is in the cache ,otherwise fetch the pokemon from the api
+    // 3 if we fetch the api , make sure to save the information to the cache for next time
+  },[selectedPokemon])
   return(
     <div></div>
   )
