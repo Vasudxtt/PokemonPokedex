8de3338e5f250e8eac87577fe0e3a949e7979f15
Vasudxtt <141990594+Vasudxtt@users.noreply.github.com>
2025-07-20 02:53:15 +0530

    update

--- src/components/TypeCard.jsx
+++ src/components/TypeCard.jsx
@@ -2,7 +2,7 @@ export default function TypeCard(props)
 {
   const {type} = props
   return(
-    <div className="type-title">
+    <div className="type-tile" style={{}}>
       <p>{type}</p>
     </div>
   )
