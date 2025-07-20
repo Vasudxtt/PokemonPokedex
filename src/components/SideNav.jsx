656c8e2878842639d6c347869824b395b4877301
Vasudxtt <141990594+Vasudxtt@users.noreply.github.com>
2025-07-21 01:08:18 +0530

    update

--- src/components/SideNav.jsx
+++ src/components/SideNav.jsx
@@ -21,7 +21,7 @@ return false
   return(
     <nav className={'' + (showSideMenu ? 'open' : '' )}>
       <div className={"header "  + (showSideMenu ? 'open' : '' )}>
-        <button onClick={handleToggleMenu} className="open-nav-button">
+        <button onClick={handleCloseMenu} className="open-nav-button">
            <i className="fa-solid fa-arrow-left-long"></i>
         </button>
         <h1 className="text-gradient">Pokédex</h1>
