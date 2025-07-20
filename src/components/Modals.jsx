ba7ea39aa0c24802b6e8a12c0fafd3fa8688d991
Vasudxtt <141990594+Vasudxtt@users.noreply.github.com>
2025-07-20 23:29:38 +0530

    update

--- src/components/Modals.jsx
+++ src/components/Modals.jsx
@@ -1,7 +1,7 @@
 import { Children } from 'react'
 import ReactDom from 'react-dom'
 
-export default function Modals(props){
+export default function Modal(props){
   const {children,handleCloseModel} = props
   return ReactDom.createPortal(
     <div className='modal-container'>
