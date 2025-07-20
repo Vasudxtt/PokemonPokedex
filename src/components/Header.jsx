85485bc14204476174eb1027dae7685f81257126
Vasudxtt <141990594+Vasudxtt@users.noreply.github.com>
2025-07-21 00:47:26 +0530

    update

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -11,6 +11,7 @@ export default function Header(props) {
       <button onClick={handleToggleMenu} className="open-nav-button">
         <i className="fa-solid fa-bars"></i>
       </button>
+      <h1 className="text-gradient">Pokedéx</h1>
     </header>
   )
 }
