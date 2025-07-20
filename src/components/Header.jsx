4f0d8441ce0ad617c487cacf71d5817ef5a5a62b
Vasudxtt <141990594+Vasudxtt@users.noreply.github.com>
2025-07-21 00:45:24 +0530

    update

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,14 @@
 export default function Header(props) {
     const { handleToggleMenu } = props
     return (
+        // <header>
+        //     <button onClick={handleToggleMenu} className="open-nav-button">
+        //         <i className="fa-solid fa-bars"></i>
+        //     </button>
+        //     <h1 className="text-gradient">Pokedéx</h1>
+        // </header>
         <header>
-            <button onClick={handleToggleMenu} className="open-nav-button">
-                <i className="fa-solid fa-bars"></i>
-            </button>
-            <h1 className="text-gradient">Pokedéx</h1>
+           <button onClick={handleToggleMenu} className="open-nav-button"></button>
         </header>
     )
   }
